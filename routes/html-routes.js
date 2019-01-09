a8fa26cef1110e616914157d99fa1e4d237312dc
Ado Moshe <adoran.moshe@gmail.com>
2019-01-09 10:34:27 -0800

    Database schema complete (user and categories)

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -11,20 +11,13 @@ module.exports = app => {
     if (req.user) {
       res.redirect('/users');
     }
-    res.sendFile(path.join(__dirname, '../public/signup.html'));
-  });
-
-  app.get('/login', (req, res) => {
-    // If the user already has an account send them to the members page
-    if (req.user) {
-      res.redirect('/users');
-    }
     res.sendFile(path.join(__dirname, '../public/login.html'));
   });
 
+
   // Here we've add our isAuthenticated middleware to this route.
   // If a user who is not logged in tries to access this route they will be redirected to the signup page
-  app.get('/members', isAuthenticated, function(req, res) {
+  app.get('/users', isAuthenticated, (req, res) => {
     res.sendFile(path.join(__dirname, '../public/users.html'));
   });
 };
