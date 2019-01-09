3f45ae6cb87950eda4b9c05e7942bd8fe3c26e5c
Ado Moshe <adoran.moshe@gmail.com>
2019-01-08 19:45:36 -0800

    Updating Passport routes

--- config/passport.js
+++ config/passport.js
@@ -1,49 +1,25 @@
-var passport = require("passport");
-var LocalStrategy = require("passport-local").Strategy;
+'use strict';
 
-var db = require("../models");
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 
-// Telling passport we want to use a Local Strategy. In other words, we want login with a username/email and password
-passport.use(new LocalStrategy(
-  // Our user will sign in using an email, rather than a "username"
-  {
-    usernameField: "email"
-  },
-  function(email, password, done) {
-    // When a user tries to sign in this code runs
-    db.User.findOne({
-      where: {
-        email: email
-      }
-    }).then(function(dbUser) {
-      // If there's no user with the given email
-      if (!dbUser) {
-        return done(null, false, {
-          message: "Incorrect email."
-        });
-      }
-      // If there is a user with the given email, but the password the user gives us is incorrect
-      else if (!dbUser.validPassword(password)) {
-        return done(null, false, {
-          message: "Incorrect password."
-        });
-      }
-      // If none of the above, return the user
-      return done(null, dbUser);
-    });
-  }
-));
+// Use the GoogleStrategy within Passport.
+//   Strategies in Passport require a `verify` function, which accept
+//   credentials (in this case, an accessToken, refreshToken, and Google
+//   profile), and invoke a callback with a user object.
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: GOOGLE_CLIENT_ID,
+      clientSecret: GOOGLE_CLIENT_SECRET,
+      callbackURL: 'http://www.example.com/auth/google/callback'
+    },
+    (accessToken, refreshToken, profile, done) => {
+      User.findOrCreate({ googleId: profile.id }, (err, user) => {
+        return done(err, user);
+      });
+    }
+  )
+);
 
-// In order to help keep authentication state across HTTP requests,
-// Sequelize needs to serialize and deserialize the user
-// Just consider this part boilerplate needed to make it all work
-passport.serializeUser(function(user, cb) {
-  cb(null, user);
-});
-
-passport.deserializeUser(function(obj, cb) {
-  cb(null, obj);
-});
-
-// Exporting our configured passport
 module.exports = passport;
--- public/javascript/signup.js
+++ public/javascript/signup.js
@@ -38,3 +38,4 @@ $(document).ready(function() {
     $("#alert").fadeIn(500);
   }
 });
+
