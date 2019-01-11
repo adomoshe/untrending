9317471a337694395dd92acc3dfe685b2965245d
swechchhaparajuli <swechchha@gmail.com>
2019-01-11 09:25:03 -0800

    it did something weird

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,15 +1,15 @@
+
 'use strict';
+
 $(document).ready(() => {
   // This file just does a GET request to figure out which user is logged in
   // and updates the HTML on the page
-  // $.get('/api/user_data').then(data => {
-  //   $('.username').text(data.displayName);
-  // });
+  $.get('/api/user_data').then(data => {
+    $('.username').text(data.displayName);
+  });
 });
 
-
-
-
+var manipulateData;
 
 ///                 TOP HEADLINES                    ///
 var frontPage;
@@ -48,7 +48,6 @@ function processData(data) {
         var artUrl = data.articles[i].url;
         console.log(artUrl);
 
-
         var $title = $("<a href=" + artUrl + '><div class="title">' + title + "</div ></a>");
         var $date = $('<div class="date">Date: ' + date + "</div >");
         var $subtitle = $("<a href=" + artUrl + '><div class="description">' + subtitle + "</div ></a>");
@@ -67,9 +66,9 @@ function processData(data) {
     }
 }
 
-
 ///                 SEARCH QUERY                   ///
 var search
+
 var url = 'https://newsapi.org/v2/everything?' +
           'q='+ search +'&' +
           'from=2019-01-10&' +
@@ -81,19 +80,70 @@ var url = 'https://newsapi.org/v2/everything?' +
 // search = $("#search-input").val();
 // console.log(search);
 
+//
+
+function showAlternativeSideNews(manipulateData){
+
+    /*
+    ** 
+    input: data from current article being viewed
+        OR
+    input: data points from search query
+        if (from search query)
+            commonPoints = find CommonViewPointShownRatio(manipulateData)
+        else 
+            commonPoints = give article a certain point
+
+    using commonPoints -- find articles from sources close the common points, and 1/4 of articles from differing POV
+
+    */
+
+    return manipulateData;
+}
+
+// pulls the average common point value of the different sources that come up as results in the search query
+function commonView(manipulateData){
+    /*
+        // put points in an array
+        // check for outliers, 
+            if there are outliers (checking for 1-3 vs 7-10 conservative level) 
+                find more frequent of the two sides, and pick the one that is more frequent
+                frequency depends on 5 results more to one side
+                    if one is more frequent then choose that and return (that common point)
+            else 
+                return (average);
+                
+    */
+}
+
+function mixSearchResults(manipulateData){
+//manipulate if there is time 
+
+    return manipulateData;
+}
+
+function chooseAlternateCountryViews(manipulateData){ 
+    /*
+        if (key words in headline, name of another country, middle east, central america, reference
+            to foreign affairs. make a db table or an array of key words for reference)
+                find other data from foreign tagged news sources
+        else    
+    */
+
+    return manipulateData;
+}
 
 
+function loosenCategoryParameters(data){
 
+    /*
+        data--> whatever interests the user has. Takes it in and 
+        manipulates (search, and top 20 methods so that you check
+        key words in the title for sports terms as well as 
+        choosing through category)
+    */
 
+    return data;
+}
 
-
-
-
-<<<<<<< HEAD
 ///                 RIP OUT SOURCES FOR FILTERING ALGORITHM                   ///
-=======
-///                 RIP OUT SOURCES FOR FILTERING ALGORITHM                   ///
-
-
-///code algorithm here///
->>>>>>> 490df8ada71f7a0a0642aae8984eb1e64f4c7c85
