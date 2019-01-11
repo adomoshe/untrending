dc2d09b749bb6f6c84edf42e5c60dd677f9d990e
ashay <albertshay888@gmail.com>
2019-01-11 10:02:46 -0800

    added click handlers

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -15,7 +15,7 @@ $(document).ready(() => {
 var frontPage;
 var queryURL = 'https://newsapi.org/v2/top-headlines?' +
     'country=us&' +
-    'apiKey=abf7b2766a1549eca7580d1b261d5838';
+    'apiKey=b0c7c3494d7f4f67aab159c80198a834';
 
 $.ajax({
     url: queryURL,
@@ -29,31 +29,38 @@ $.ajax({
 });
 
 function processData(data) {
-    // var articleItems = [];
+    var articleItems = [artTitle, artSource, artDescription, datePublished, artContent, artImage, artUrl];
 
     for (var i = 0; i < data.articles.length; i++) {
 
-        var title = data.articles[i].title;
-        console.log(title);
+        var artTitle = data.articles[i].title;
+        console.log(artTitle);
 
-        var subtitle = data.articles[i].description;
-        console.log(subtitle);
+        var artSource = data.articles[i].source.name;
+        console.log(artSource); 
 
-        var date = data.articles[i].publishedAt;
-        console.log(date);
+        var artDescription = data.articles[i].description;
+        console.log(artDescription);
 
-        var blurb = data.articles[i].content;
-        console.log(blurb);
+        var datePublished = data.articles[i].publishedAt;
+        console.log(datePublished);
+
+        var artContent = data.articles[i].content;
+        console.log(artContent);
+
+
+        var artImage = data.articles[i].urlToImage;
+        console.log(artImage);
 
         var artUrl = data.articles[i].url;
         console.log(artUrl);
 
 
-        var $title = $("<a href=" + artUrl + '><div class="title">' + title + "</div ></a>");
-        var $date = $('<div class="date">Date: ' + date + "</div >");
-        var $subtitle = $("<a href=" + artUrl + '><div class="description">' + subtitle + "</div ></a>");
-        var $blurb = $('<div class="blurb">' + blurb + "</div >");
-        $(".front-page-feed").append($title, $subtitle, $date, $blurb);
+        var $artTitle = $("<a href=" + artUrl + '><div class="title">' + artTitle + "</div ></a>");
+        var $datePublished = $('<div class="date">Date: ' + datePublished + "</div >");
+        var $artDescription = $("<a href=" + artUrl + '><div class="description">' + subtitle + "</div ></a>");
+        var $artContent = $('<div class="blurb">' + artContent + "</div >");
+        $(".front-page-feed").append($artTitle, $artDescription, $datePublihed, $artContent);
         console.log(artUrl);
     }
 
@@ -64,22 +71,47 @@ function processData(data) {
         var $thumbnail = $("<img class ='thumbnail' src=" + thumbnail + ">")
 
         $(".thumbnail-feed").append($thumbnail)
-    }
+    }   
 }
 
 
 ///                 SEARCH QUERY                   ///
-var search
+var search;
 var url = 'https://newsapi.org/v2/everything?' +
           'q='+ search +'&' +
           'from=2019-01-10&' +
           'sortBy=popularity&' +
-          'apiKey=abf7b2766a1549eca7580d1b261d5838';
+          'apiKey=b0c7c3494d7f4f67aab159c80198a834';
+
+
+
+
+search = $("#search-input").val();
+console.log(search);
+
+// CLICK HANDLERS
+// ==========================================================
 
 // on click of submit button, user input = search, and call processData. 
-
-// search = $("#search-input").val();
-// console.log(search);
+$("#search-input").on("click", function(event) {
+   
+    event.preventDefault();
+  
+ 
+  
+    // Build the query URL for the ajax request to the NYT API
+    var queryURL = buildQueryURL();
+  
+    // Make the AJAX request to the API - GETs the JSON data at the queryURL.
+    // The data then gets passed as an argument to the processData function
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(processData);
+  });
+  
+  //  .on("click") function associated with the clear button
+  $("#clear-all").on("click", clear);
 
 
 
