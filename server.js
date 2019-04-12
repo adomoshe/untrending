623f1312fa08f0a7998c57fc20c84276730e4c81
Ado Moshe <adoran.moshe@gmail.com>
2019-04-11 23:46:56 -0700

    Working on Sequelize deprecation issue

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const routes = require('./routes');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
-app.use(session({ secret: 'chicken', secure: 'auto' }));
+// app.use(session({ secret: 'chicken', secure: 'auto' }));
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(routes);
